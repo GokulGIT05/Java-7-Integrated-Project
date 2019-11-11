8e6fe0617b7a925c1bcfb8500fe2bd1619cf5187
gokul <gokul.g@aa.com>
2019-11-11 17:11:36 +0530

    Thread Methods - yield, join, sleep, interrupt

---
+++ src/com/gk7/thread/methods/Gk4ThreadJoinMethod2.java
@@ -0,0 +1,38 @@
+package com.gk7.thread.methods;
+
+public class Gk4ThreadJoinMethod2 {
+
+	// To Run Main Thread First and Child Thread Second
+	public static void main(String[] args) {
+
+		JoinThreadDemo1.mainThread = Thread.currentThread(); 
+		
+		JoinThreadDemo1 joinObj = new JoinThreadDemo1();
+		Thread joinThread = new Thread(joinObj);
+		joinThread.start();
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Inside Main Method");
+		}
+	}
+}
+
+class JoinThreadDemo1 implements Runnable {
+	
+	public static Thread mainThread = null;
+	
+	@Override
+	public void run() {
+		
+		try {
+			mainThread.join();  // child thread calls this join on mainThread
+			// First Main Method will execute
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Inside Run() Method: ");
+		}
+	}
+}
---
+++ src/com/gk7/thread/methods/Gk6ThreadInterrupt.java
@@ -0,0 +1,35 @@
+package com.gk7.thread.methods;
+
+public class Gk6ThreadInterrupt {
+
+	public static void main(String[] args) {
+
+		InterruptThreadDemo threadDemo = new InterruptThreadDemo();
+		threadDemo.start();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Inside Main Method");
+		}
+		
+		threadDemo.interrupt();
+
+	}
+
+}
+
+class InterruptThreadDemo extends Thread {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Inside run Method");
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println("Child Thread is Interrupted: "+e.toString());
+		}
+	}
+}
