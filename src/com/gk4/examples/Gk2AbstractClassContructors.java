6a22dab085fb576cfde28057b95f7b5b4d23b111
Gokul - Tortoise <gkgopig02@gmail.com>
2019-11-16 16:15:29 +0530

    Example of Abstract VS Interface

---
+++ src/com/gk4/examples/Gk2AbstractClassContructors.java
@@ -0,0 +1,49 @@
+package com.gk4.examples;
+
+public class Gk2AbstractClassContructors {
+
+	public static void main(String[] args) {
+
+		// Only Child Class Object is created. Parent Abstract class constructor is used
+		// to initialize the parent class instance varaibles for child class object
+		ChildClassForDemo childObj = new ChildClassForDemo("Gokul", 26, "TVL", "TN", "IT", "Software Developer");
+		System.out.println(childObj);
+
+	}
+
+}
+
+abstract class ParentAbstractClass {
+
+	String name;
+	int age;
+	String district;
+	String state;
+
+	public ParentAbstractClass(String name, int age, String district, String state) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.district = district;
+		this.state = state;
+	}
+}
+
+class ChildClassForDemo extends ParentAbstractClass {
+
+	String occupation;
+	String role;
+
+	public ChildClassForDemo(String name, int age, String district, String state, String occupation, String role) {
+		super(name, age, district, state);
+		this.occupation = occupation;
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "ChildClassForDemo [occupation=" + occupation + ", role=" + role + ", name=" + name + ", age=" + age
+				+ ", district=" + district + ", state=" + state + "]";
+	}
+
+}
