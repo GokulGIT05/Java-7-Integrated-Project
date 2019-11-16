37e5a223217431d34d17c973d929f0df7b3f90e7
Gokul - Tortoise <gkgopig02@gmail.com>
2019-11-16 23:03:58 +0530

    Inner Class First Part

---
+++ src/com/gk8/innerclass/Gk1NormalInnerClassDemo.java
@@ -0,0 +1,21 @@
+package com.gk8.innerclass;
+
+public class Gk1NormalInnerClassDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Inside Inner Class");
+		
+		
+
+	}
+	
+	class InnerClassDemo{
+		// Inside Inner Class, we can't declare static methods.
+		
+		public void innerClassMethod() {
+			
+		}
+		
+	}
+
+}
